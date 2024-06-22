0362130d831e80860d14ddd0b142e6b2a39261d8
Christian Heinisch <chris@christian-heinisch.de>
2024-06-22 16:27:32 +0200

    Update journal.php

---
+++ journal.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Jornal Diary</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.19.4/css/uikit.min.css" />
+  </head>
+  <body>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.19.4/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.19.4/js/uikit-icons.min.js"></script>
+  </body>
+</html>
