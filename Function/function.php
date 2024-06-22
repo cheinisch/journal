3771266b0142ad12fb07edd3a71ed66aa8227842
Christian Heinisch <chris@christian-heinisch.de>
2024-06-22 16:23:33 +0200

    Update Cookie settings

--- Function/function.php
+++ Function/function.php
@@ -5,6 +5,9 @@ $cookie_value = 'username';
 
 function check_session()
 {
+    $cookie_name = 'diarycms';
+    $cookie_value = 'username';
+
     if(!isset($_COOKIE[$cookie_name])) {
         return false;
       } else {
@@ -13,6 +16,9 @@ function check_session()
 }
 function create_session()
 {
+    $cookie_name = 'diarycms';
+    $cookie_value = 'username';
+
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 }
 
