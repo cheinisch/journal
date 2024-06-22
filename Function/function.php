242a9bdbe6e6c6a0a7ae2e3e6abce15d9fbae018
Heimfisch <chris@christian-heinisch.de>
2024-06-22 21:37:10 +0200

    change to db

--- Function/function.php
+++ Function/function.php
@@ -30,66 +30,116 @@ function destroy_session()
     
 }
 
-function findMarkdownFiles($directory) {
-    $markdownFiles = [];
-    
-    $dirIterator = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
-    $iterator = new RecursiveIteratorIterator($dirIterator, RecursiveIteratorIterator::SELF_FIRST);
-    
-    foreach ($iterator as $fileInfo) {
-        if ($fileInfo->isFile() && $fileInfo->getExtension() === 'md') {
-            $markdownFiles[] = $fileInfo->getPathname();
-        }
-    }
-    
-    return $markdownFiles;
-}
-
 function getLogin($username, $password)
  {
     return true;
  }
 
 
-function loadMarkdownPosts($directory = './../storage/2024-06-22/') {
-  $posts = [];
-  $files = findMarkdownFiles($directory);
+/**
+ * Erstellt einen neuen Blogpost in der Datenbank.
+ *
+ * @param string $title Titel des Blogposts.
+ * @param string $content Inhalt des Blogposts.
+ * @param string $author Name des Autors.
+ * @param string $date Veröffentlichungsdatum im Format `Y-m-d`.
+ * @param string $location Ortsangaben des Blogposts.
+ * @param array $tags Tags des Blogposts.
+ * @return int Die ID des neu erstellten Blogposts.
+ */
+function createBlogPost($title, $content, $author, $date, $location = null, $tags = []) {
+  $db = getDatabaseConnection();
+  $sql = "INSERT INTO blog_posts (title, content, author, date, location, tags) VALUES (:title, :content, :author, :date, :location, :tags)";
+  $stmt = $db->prepare($sql);
+  $stmt->execute([
+      ':title' => $title,
+      ':content' => $content,
+      ':author' => $author,
+      ':date' => $date,
+      ':location' => $location,
+      ':tags' => json_encode($tags) // Tags als JSON speichern
+  ]);
+  return $db->lastInsertId();
+}
 
-  $parsedown = new Parsedown();
+/**
+* Bearbeitet einen bestehenden Blogpost in der Datenbank.
+*
+* @param int $postId Die ID des Blogposts.
+* @param string|null $newTitle Neuer Titel (optional).
+* @param string|null $newContent Neuer Inhalt (optional).
+* @param string|null $newAuthor Neuer Autor (optional).
+* @param string|null $newDate Neues Datum (optional).
+* @param string|null $newLocation Neuer Ort (optional).
+* @param array|null $newTags Neue Tags (optional).
+* @return bool True, wenn der Blogpost aktualisiert wurde, ansonsten false.
+*/
+function editBlogPost($postId, $newTitle = null, $newContent = null, $newAuthor = null, $newDate = null, $newLocation = null, $newTags = null) {
+  $db = getDatabaseConnection();
+  $fields = [];
+  $params = [':id' => $postId];
 
-
-  foreach ($files as $file) {
-
-      
-
-      // Beispiel: /storage/2024-06-21/post-name.md
-      $pathParts = explode('/', $file);
-      $fileName = array_pop($pathParts);
-      $datePart = array_slice($pathParts, -1)[0];
-
-      if (preg_match('/(\d{4})-(\d{2})-(\d{2})/', $datePart, $matches)) {
-          $date = $matches[0];
-      } else {
-          continue; // Dateiformat nicht erkannt
-      }
-
-      $title = basename($file, '.md');
-      $content = file_get_contents($file);
-      $htmlContent = $parsedown->text($content);
-
-      $posts[] = [
-          'title' => ucwords(str_replace('-', ' ', $title)),
-          'date' => $date,
-          'content' => $htmlContent
-      ];
+  if ($newTitle !== null) {
+      $fields[] = 'title = :title';
+      $params[':title'] = $newTitle;
+  }
+  if ($newContent !== null) {
+      $fields[] = 'content = :content';
+      $params[':content'] = $newContent;
+  }
+  if ($newAuthor !== null) {
+      $fields[] = 'author = :author';
+      $params[':author'] = $newAuthor;
+  }
+  if ($newDate !== null) {
+      $fields[] = 'date = :date';
+      $params[':date'] = $newDate;
+  }
+  if ($newLocation !== null) {
+      $fields[] = 'location = :location';
+      $params[':location'] = $newLocation;
+  }
+  if ($newTags !== null) {
+      $fields[] = 'tags = :tags';
+      $params[':tags'] = json_encode($newTags); // Tags als JSON speichern
   }
 
-  // Sortiere die Posts nach Datum (neuste zuerst)
-  usort($posts, function($a, $b) {
-      return strtotime($b['date']) - strtotime($a['date']);
-  });
+  $sql = "UPDATE blog_posts SET " . implode(', ', $fields) . " WHERE id = :id";
+  $stmt = $db->prepare($sql);
+  return $stmt->execute($params);
+}
 
-  return $posts;
+/**
+* Liest einen Blogpost aus der Datenbank aus.
+*
+* @param int $postId Die ID des Blogposts.
+* @return array|null Array mit `id`, `title`, `content`, `author`, `date`, `location`, `tags` oder null, wenn nicht gefunden.
+*/
+function readBlogPost($postId) {
+  $db = getDatabaseConnection();
+  $sql = "SELECT * FROM blog_posts WHERE id = :id";
+  $stmt = $db->prepare($sql);
+  $stmt->execute([':id' => $postId]);
+  $post = $stmt->fetch();
+  if ($post) {
+      $post['tags'] = json_decode($post['tags'], true); // Tags von JSON in ein Array umwandeln
+  }
+  return $post;
+}
+
+
+
+/**
+ * Löscht einen Blogpost aus der Datenbank.
+ *
+ * @param int $postId Die ID des Blogposts.
+ * @return bool True, wenn der Blogpost gelöscht wurde, ansonsten false.
+ */
+function deleteBlogPost($postId) {
+  $db = getDatabaseConnection();
+  $sql = "DELETE FROM blog_posts WHERE id = :id";
+  $stmt = $db->prepare($sql);
+  return $stmt->execute([':id' => $postId]);
 }
 
 ?>
