4ea9236085b8d3a127f7db7ee568ed41f29f3749
Christian Heinisch <chris@christian-heinisch.de>
2024-07-21 19:56:15 +0200

    save new entry
    modify save data

--- system/gui/edit.php
+++ system/gui/edit.php
@@ -41,34 +41,34 @@
         <div uk-grid>
             <!-- Blogposts -->
             <div class="uk-width-2-3@s uk-width-1-1">
-            <form action="save.php" method="get" class="uk-grid-small" uk-grid>
+            <form class="uk-grid-small" uk-grid action="system/function/save.php" method="get" >
 
                     <legend class="uk-legend">Neuer Eintrag</legend>
 
                     <div class="uk-width-1-1">
-                        <input class="uk-input" type="text" placeholder="Headline" aria-label="Input">
+                        <input id="headline" name="headline" class="uk-input" type="text" placeholder="Headline" aria-label="Input">
                     </div>
 
                     <div class="uk-width-1-2@s">
                         <label for="date" class="uk-form-label">Datum auswählen:</label>
                         <div class="uk-form-controls">
-                            <input id="date" class="uk-input" type="date" value="<?php echo date("Y-m-d");?>">
+                            <input id="date" name="date" class="uk-input" type="date" value="<?php echo date("Y-m-d");?>">
                         </div>
                     </div>
                     <div class="uk-width-1-4@s">
                         <label for="time" class="uk-form-label">Uhrzeit auswählen:</label>
                         <div class="uk-form-controls">
-                            <input id="time" class="uk-input" type="time" value="<?php echo date("H:i");?>">
+                            <input id="time" name="time" class="uk-input" type="time" value="<?php echo date("H:i");?>">
                         </div>
                     </div>
 
 
                     <div class="uk-width-1-1">
-                        <textarea class="uk-textarea" rows="5" placeholder="Textarea" aria-label="Textarea"></textarea>
+                        <textarea id="text" name="text" class="uk-textarea" rows="5" placeholder="Textarea" aria-label="Textarea"></textarea>
                     </div>
 
                     <div class="uk-width-1-1">
-                    <button class="uk-button uk-button-primary" type="submit">Save</button>
+                    <button class="uk-button uk-button-primary" type="submit" value="submit">Save</button>
                     </div>
 
             </form>              
